6eb6bd0441798d0250d603bc0f8fe490e474ef46
bannerl <15756026551@163.com>
2017-02-07 20:24:51 +0800

    add four page

--- phone/js/js.js
+++ phone/js/js.js
@@ -1,7 +1,42 @@
 var highpage = document.getElementById("highpage");
 var uppage = document.getElementsByClassName("first-page");
 var currentwidth = parseInt(getStyle(uppage[0]).width);
+var userInfo = document.getElementsByClassName("user-info")[0];
 
+//创建标签
+function newele(){
+	
+}
+//后台数据请求
+function getInfo(){
+
+	if(window.XMLHttpRequest){
+		var xhr = new XMLHttpRequest();
+	}else{
+		var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xhr.open("GET","js/user.json",true);
+	xhr.send();
+	
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4&&xhr.status === 200){
+			var text = JSON.parse(xhr.responseText);
+			var ele1 = document.createElement("p");
+			var content1 = document.createTextNode(text[0].name);
+			ele1.className = "name";
+			ele1.appendChild(content1);
+			var ele2 = document.createElement("p");
+			var content2 = document.createTextNode("微信号："+text[0].number);
+			ele2.className = "number";
+			ele2.appendChild(content2);
+			userInfo.appendChild(ele1);
+			userInfo.appendChild(ele2);
+		}
+	};
+
+}
+getInfo();
+//触摸事件
 document.addEventListener("touchstart",function(e){
 	e.preventDefault();
 	var touch = e.touches[0];
