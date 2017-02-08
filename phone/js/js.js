a2886d3300935c1e44099920622164389c62de57
bannerl <15756026551@163.com>
2017-02-08 17:06:16 +0800

    last change

--- phone/js/js.js
+++ phone/js/js.js
@@ -3,31 +3,55 @@ var uppage = document.getElementsByClassName("first-page");
 var currentwidth = parseInt(getStyle(uppage[0]).width);
 var userInfo = document.getElementsByClassName("user-info")[0];
 var footerCtrl = document.getElementsByClassName("footer-ctrl");
+var time = document.getElementsByClassName("time")[0];
+//获取当前时间
+setInterval(function(){
+	var date = new Date();
+	var hour = date.getHours();
+	var second = date.getSeconds();
+	var minute = date.getMinutes();
+	if(hour-10<0){
+		hour = "0"+hour;
+	}
+	if(second-10<0){
+		second = "0" + second; 
+	}
+	if(minute-10<0){
+		minute = "0" + minute;
+	}
+	time.innerHTML = hour+":"+minute+":"+second;
+},500);
+
 //footer控制
-var boo = false;
 for(var n = 0;n<footerCtrl.length;n++){
 	footerCtrl[n].index = n;
 	footerCtrl[n].getElementsByClassName("icon")[1].style.display = "none";
+	footerCtrl[0].getElementsByClassName("icon")[1].style.display = "inline-block";
+	footerCtrl[0].getElementsByClassName("icon")[0].style.display = "none";
 	footerCtrl[n].onclick = function(){
-		for(var ns = 0;ns<footerCtrl.length;ns++){
-			footerCtrl[ns].getElementsByClassName("icon")[1].style.display = "none";
-			footerCtrl[ns].getElementsByClassName("icon")[0].style.display = "inline-block";
-		}
-		//active切换
-		var _all = footerCtrl[this.index].getElementsByClassName("icon");
-		if(getStyle(_all[0]).display=="none"){
-			_all[0].style.display = "inline-block";
-			_all[1].style.display = "none";
-		}else{
-			_all[1].style.display = "inline-block";
-			_all[0].style.display = "none";
-		}	
-		var len = -(this.index*currentwidth);
-		boo = true;
-		highpage.style.webkitTransform = "translate3d("+len+"px,0,0)";
+		var page = pageChange(this.index);
+		highpage.style.webkitTransform = "translate3d("+page+"px,0,0)";
 	}
 }
-
+//控制页面函数
+function pageChange(index){
+	for(var ns = 0;ns<footerCtrl.length;ns++){
+		footerCtrl[ns].className="footer-ctrl";
+		footerCtrl[ns].getElementsByClassName("icon")[1].style.display = "none";
+		footerCtrl[ns].getElementsByClassName("icon")[0].style.display = "inline-block";
+	}
+	//active切换
+	footerCtrl[index].className+=" active";
+	var _all = footerCtrl[index].getElementsByClassName("icon");
+	if(getStyle(_all[0]).display=="none"){
+		_all[0].style.display = "inline-block";
+		_all[1].style.display = "none";
+	}else{
+		_all[1].style.display = "inline-block";
+		_all[0].style.display = "none";
+	}
+	return -(index*currentwidth);
+}
 //后台数据请求
 function getInfo(){
 	//兼容ie6
@@ -60,7 +84,6 @@ getInfo();
 //触摸事件
 document.addEventListener("touchstart",function(e){
 	//e.preventDefault();
-	console.log(typeof e.path[1].onclick);
 	var touch = e.touches[0];
 	startX = touch.pageX;
 	startY = touch.pageY;
@@ -77,7 +100,7 @@ document.addEventListener("touchmove",function(e){
 	 deltaX = touch.pageX - startX;
 	 deltaY = touch.pageY - startY;
 	//如果向左右位移大于上下则是左右位移
-	if(Math.abs(deltaX)>Math.abs(deltaY)){
+	if(Math.abs(deltaX)>Math.abs(deltaY)&&Math.abs(deltaX)>30){
 		nes = parseFloat(nes);
 		var len = nes + deltaX;
 	 	if(len <=-1080){
@@ -89,9 +112,12 @@ document.addEventListener("touchmove",function(e){
 	}
 });
 var nes = 0;
-
+ var lens;
 document.addEventListener("touchend",function(e){
-	 var lens;
+	//判断是否有点击事件
+	if(e.path.length == "9"||e.path.length == "10"){
+		return false;
+	}
 	/*判断移动距离是否满足*/ 
 	if(Math.abs(deltaX)>currentwidth/4){
 		for(var i = 0;i<uppage.length;i++){
@@ -113,19 +139,15 @@ document.addEventListener("touchend",function(e){
 			}
 		}
 	}else{
-		console.log(boo);
-		if(boo){
-			return false;
-		}else{
-			console.log(1);
-			lens = nes;
-		}
-	}	
-	console.log(21);
+		lens = nes;
+	}
+	var _lens = Math.abs(lens)/375;
+	
+	console.log(_lens);
+	pageChange(_lens);
 	highpage.style.webkitTransform = "translate3d("+lens+"px,0,0)";
 	nes = highpage.style.webkitTransform.slice("12").split("px")[0];	
 });
-
 
 function getStyle(ele) {
     var style = null;
